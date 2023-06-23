8d4d6b5d3cf2bf96d8dfbbb26253b1f78838446b
Fet2113 <super.gjoll@yandex.ru>
2023-06-23 19:13:34 +0300

    commit Ошибка

--- src/main/java/pro/sky/skyprospringcalculator/CalculatorService.java
+++ src/main/java/pro/sky/skyprospringcalculator/CalculatorService.java
@@ -1,6 +1,27 @@
 package pro.sky.skyprospringcalculator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public interface CalculatorService {
-    String hello();
-    String answerHello();
+    @GetMapping
+    public String hello();
+
+    @GetMapping
+    public String answerHello();
+
+    @GetMapping
+    public String plus(@RequestParam("num1") Integer numOne,
+                       @RequestParam("num2") Integer numTwo);
+
+    String minus(@RequestParam(required = false) Integer num1,
+                 @RequestParam(required = false) Integer num2);
+
+
+ /*   public String multiply(@RequestParam(required = false) Integer num1,
+                           @RequestParam(required = false) Integer num2);
+
+
+    public String divide(@RequestParam(required = false) Integer num1,
+                         @RequestParam(required = false) Integer num2); */
 }
